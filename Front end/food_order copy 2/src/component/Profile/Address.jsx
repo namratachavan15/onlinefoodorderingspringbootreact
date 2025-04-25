e6bf0f0cd4eb35041018bea083cb54e45564a1b1
namratachavan15 <141033807+namratachavan15@users.noreply.github.com>
2025-04-25 18:42:53 +0530

    Update Address.jsx

--- Front end/food_order copy 2/src/component/Profile/Address.jsx
+++ Front end/food_order copy 2/src/component/Profile/Address.jsx
@@ -13,35 +13,26 @@ const Address = () => {
       </Container>
     );
   }
-
+ 
   return (
     <Container fluid className="bg-dark text-white min-vh-1000  py-5" style={{height:'800px'}} >
       <h3 className="text-center border-bottom pb-3 mb-4">Your Addresses</h3>
-      <Row className="g-4 justify-content-center px-3">
-        {user.address.map((address) => (
-          <Col key={address.id} xs="12" sm="6" md="4" lg="3">
-            <Card className=" text-white h-100  shadow-sm" style={{width:'250px',backgroundColor:'#191919'}}>
-              <CardBody>
-                <CardText>
-                  <strong>Street Address:</strong> {address.streetAddress}
-                </CardText>
-                <CardText>
-                  <strong>City:</strong> {address.city}
-                </CardText>
-                <CardText>
-                  <strong>State:</strong> {address.state}
-                </CardText>
-                <CardText>
-                  <strong>Postal Code:</strong> {address.postalCode}
-                </CardText>
-                <CardText>
-                  <strong>Country:</strong> {address.country}
-                </CardText>
-              </CardBody>
-            </Card>
-          </Col>
-        ))}
-      </Row>
+      <Row className="gx-4 gy-4 justify-content-center px-2">
+  {user.address.map((address) => (
+    <Col key={address.id} xs="12" sm="10" md="6" lg="4" xl="3" className="d-flex justify-content-center">
+      <Card className="text-white h-100 shadow-sm" style={{ width: '100%', maxWidth: '250px', backgroundColor: '#191919' }}>
+        <CardBody>
+          <CardText><strong>Street Address:</strong> {address.streetAddress}</CardText>
+          <CardText><strong>City:</strong> {address.city}</CardText>
+          <CardText><strong>State:</strong> {address.state}</CardText>
+          <CardText><strong>Postal Code:</strong> {address.postalCode}</CardText>
+          <CardText><strong>Country:</strong> {address.country}</CardText>
+        </CardBody>
+      </Card>
+    </Col>
+  ))}
+</Row>
+
     </Container>
   );
 };
