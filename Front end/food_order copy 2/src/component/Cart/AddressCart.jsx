cfbac6872ef7fd7daf6da676b872fae98a694fed
namratachavan15 <141033807+namratachavan15@users.noreply.github.com>
2025-04-28 17:23:04 +0530

    Update AddressCart.jsx

--- Front end/food_order copy 2/src/component/Cart/AddressCart.jsx
+++ Front end/food_order copy 2/src/component/Cart/AddressCart.jsx
@@ -4,7 +4,7 @@ import { FaHome, FaMapMarkerAlt } from 'react-icons/fa';
 
 const AddressCart = ({ item, showButton, handleSelectAddress }) => {
   return (
-    <Card className="d-flex flex-row gap-3 p-4 w-64"  style={{backgroundColor:"#191919"}}>
+    <Card className="d-flex flex-row gap-3 p-4 w-100 flex-wrap"  style={{backgroundColor:"#191919"}}>
       <FaHome size={35} className="text-white" />
       <div className="space-y-3 text-gray-500">
         <h1 className="font-semibold text-sm" style={{ fontFamily: "Roboto, Helvetica, Arial, sans-serif" }}>Mumbai, New Shivam Building, Gokuldham Market, Maha India</h1>
