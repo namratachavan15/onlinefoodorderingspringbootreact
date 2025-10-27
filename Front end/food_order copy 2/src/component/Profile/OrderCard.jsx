0389bc5d1c4b15b321e9411e1b48d97b1f0f5c34
namratachavan15 <141033807+namratachavan15@users.noreply.github.com>
2025-10-27 17:54:23 +0530

    Update OrderCard.jsx

--- Front end/food_order copy 2/src/component/Profile/OrderCard.jsx
+++ Front end/food_order copy 2/src/component/Profile/OrderCard.jsx
@@ -1,32 +1,35 @@
-import React from 'react'
-import { Card, CardBody, Button } from 'reactstrap'
-import { FaCheckCircle } from 'react-icons/fa'
+import React from 'react';
+import { Card, CardBody, Button } from 'reactstrap';
+import { FaCheckCircle } from 'react-icons/fa';
 
-const OrderCard = ({item,orders}) => {
-  console.log('Orders:',JSON.stringify(orders));
+const OrderCard = ({ item, orders }) => {
   return (
-    <Card className='d-flex justify-content-between align-items-center w-100 p-1 mb-4 bg-black'>
-      <CardBody className='d-flex justify-content-between w-100 bg-black'>
-        <div className='d-flex align-items-center'>
-          <img 
-            className='h-16 w-16 mr-4'
-            src={item.food.images[0]} 
-            alt="Pizza"
-          />
-          <div style={{color:'white'}}>
-            <p>{item.food.name}</p>
-            <p>${item.totalPrice}</p>
-          </div>
-        </div>
+    <Card className="mb-3 text-white w-100" style={{ backgroundColor: '#191919' }}>
+    <CardBody className="d-flex flex-column flex-md-row justify-content-between align-items-start align-items-md-center">
+      <div className="d-flex align-items-start">
+        <img
+          className="img-fluid me-3"
+          src={item.food.images[0]}
+          alt="Food"
+          style={{ width: '60px', height: '60px', objectFit: 'cover', borderRadius: '5px' }}
+        />
         <div>
-          
-          <Button className='mt-3' disabled style={{color:"#e91e63",backgroundColor:'black' }}>
-             {orders.orderStatus}
-          </Button>
+          <p className="mb-1 fw-bold">{item.food.name}</p>
+          <p className="mb-0">₹{item.totalPrice}</p>
         </div>
-      </CardBody>
-    </Card>
-  )
-}
+      </div>
+      <div className="mt-3 mt-md-0">
+        <Button
+          className="border-0"
+          style={{ color: '#e91e63', backgroundColor: 'black' }}
+        >
+          {orders.orderStatus}
+        </Button>
+      </div>
+    </CardBody>
+  </Card>
+  
+  );
+};
 
-export default OrderCard
+export default OrderCard;
