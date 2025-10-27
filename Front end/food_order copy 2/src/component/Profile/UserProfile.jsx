96839bb2aa00ff2e2633e0969a9df41a611e5fd7
namratachavan15 <141033807+namratachavan15@users.noreply.github.com>
2025-10-27 18:09:50 +0530

    Update UserProfile.jsx

--- Front end/food_order copy 2/src/component/Profile/UserProfile.jsx
+++ Front end/food_order copy 2/src/component/Profile/UserProfile.jsx
@@ -1,21 +1,31 @@
 import React from 'react';
-import { Button } from 'reactstrap'; // Reactstrap Button
-import { FaUserCircle } from 'react-icons/fa'; // React Icon for user profile icon
+import { Button } from 'reactstrap';
+import { FaUserCircle } from 'react-icons/fa';
+import { useAuth } from '../Auth/AuthContext';
+import { useNavigate } from 'react-router-dom';
 
 const UserProfile = () => {
-  const handleLogout = () => {
-    // Handle logout functionality here
-  };
+  const { user, logout } = useAuth();
+  const navigate = useNavigate();
+
+  const handleLogout = () => logout(navigate);
 
   return (
-    <div className=" d-flex flex-column justify-content-center align-items-center text-center">
+    <div className="d-flex flex-column justify-content-center align-items-center text-center">
       <div className="d-flex flex-column justify-content-center align-items-center">
-        {/* React Icons User Icon */}
+        
         <FaUserCircle style={{ fontSize: '9rem' }} />
-        <h1 className="py-3 text-2xl font-weight-bold">Code with Zosh</h1>
-        <p>Email: codewithzosh@gmail.com</p>
-        {/* Reactstrap Button */}
-        <Button onClick={handleLogout} style={{ margin: '2rem 0',backgroundColor:"#e91e63" }}>
+
+        <h1 className="py-3 text-2xl font-weight-bold">
+          {user?.fullName || "Anonymous User"}
+        </h1>
+
+        <p>Email: {user?.email || "Not Available"}</p>
+
+        <Button
+          onClick={handleLogout}
+          style={{ margin: '2rem 0', backgroundColor: "#e91e63" }}
+        >
           Logout
         </Button>
       </div>
