674d8ff4a2ff3e65fb785b10d77f5d189404bd4f
namratachavan15 <141033807+namratachavan15@users.noreply.github.com>
2025-11-05 07:51:11 +0530

    Update Routers.jsx

--- Front end/food_order copy 2/src/component/Routers/Routers.jsx
+++ Front end/food_order copy 2/src/component/Routers/Routers.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 import { Routes,Route } from 'react-router-dom';
 import AdminRoute from './AdminRoute';
 import CustomerRouter from './CustomerRouter';
+import Navbar from '../Navbar/Navbar';
 const Routers = () => {
   return (
-   
+   <div>
+    <Navbar/>
       <Routes>
       <Route path='/admin/restaurant/*' element={<AdminRoute/>}></Route>
       <Route path='/*' element={<CustomerRouter/>}></Route>
     </Routes>
-    
+    </div>
   )
 }
 
